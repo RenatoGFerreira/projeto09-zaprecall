19539ba59c515f5b7e0ea6c03ed00a2948cd9a3e
RenatoUbuntu <renatoyferreira@gmail.com>
2022-12-02 11:05:13 -0300

    cards funcionando

--- src/Cards.js
+++ src/Cards.js
@@ -1,10 +1,20 @@
 import Card from "./Card"
-
+import { useState } from "react"
 
 export default function Cards({cards}) {
+
+    const [ cartaAberta, setCartaAberta] = useState(null)
+
     return (
         <>
-            {cards.map((cards, index) => (<Card key={index} indice={index}/>))}
+            {cards.map((cards, index) => (
+            <Card 
+                key={index} 
+                indice={index}
+                abrirCarta={()=>setCartaAberta(index)}
+                abriuCarta={index === cartaAberta}
+                cards={cards}
+            />))}
         </>
     )
 }
